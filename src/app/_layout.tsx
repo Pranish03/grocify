354830819e472a99cbb0e7fdd724ff4c0e399d6b
Pranish Chaulagain <153913535+Pranish03@users.noreply.github.com>
2026-08-18 14:26:22 +0545

    feat: remove unused color variables and theme provider from Tailwind CSS configuration and layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,6 @@
 import { ClerkProvider } from "@clerk/expo";
 import { tokenCache } from "@clerk/expo/token-cache";
-import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from "expo-router";
-import { useColorScheme } from "react-native";
+import { Stack } from "expo-router";
 import "../../global.css";
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
@@ -11,13 +10,9 @@ if (!publishableKey) {
 }
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <Stack screenOptions={{ headerShown: false }} />
-      </ThemeProvider>
+      <Stack screenOptions={{ headerShown: false }} />
     </ClerkProvider>
   );
 }
