afe4f10219649e6cc92fc003b02c99c0e5080e77
Pranish Chaulagain <153913535+Pranish03@users.noreply.github.com>
2026-08-19 15:00:15 +0545

    feat(tabs): add TabsLayout, HomeScreen, InsightsScreen, and PlannerScreen components

---
+++ src/app/(tabs)/insights.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function InsightsScreen() {
+  return (
+    <View>
+      <Text>InsightsScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ src/app/(tabs)/planner.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const PlannerScreen = () => {
+  return (
+    <View>
+      <Text>PlannerScreen</Text>
+    </View>
+  );
+};
+
+export default PlannerScreen;
