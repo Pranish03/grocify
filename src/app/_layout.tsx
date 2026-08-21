a66bf6d82ef9c781b712cf72ebcfd1a385c452ad
Pranish Chaulagain <153913535+Pranish03@users.noreply.github.com>
2026-08-21 15:32:47 +0545

    feat(auth): integrate AuthSession for social authentication redirect handling

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,10 @@
 import { ClerkProvider } from "@clerk/expo";
 import { tokenCache } from "@clerk/expo/token-cache";
 import { Stack } from "expo-router";
+import * as WebBrowser from "expo-web-browser";
 import "../../global.css";
+
+WebBrowser.maybeCompleteAuthSession();
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
--- src/hooks/useSocialAuth.ts
+++ src/hooks/useSocialAuth.ts
@@ -1,4 +1,5 @@
 import { useSSO } from "@clerk/expo";
+import * as AuthSession from "expo-auth-session";
 import { useState } from "react";
 import { Alert } from "react-native";
 
@@ -14,7 +15,10 @@ const useSocialAuth = () => {
     setLoadingStrategy(strategy);
 
     try {
-      const { createdSessionId, setActive } = await startSSOFlow({ strategy });
+      const { createdSessionId, setActive } = await startSSOFlow({
+        strategy,
+        redirectUrl: AuthSession.makeRedirectUri(),
+      });
 
       if (!createdSessionId || !setActive) {
         Alert.alert(
@@ -26,7 +30,7 @@ const useSocialAuth = () => {
 
       await setActive({ session: createdSessionId });
     } catch (error) {
-      console.log("Error in aocial auth: ", error);
+      console.log("Error in social auth: ", error);
       Alert.alert("Error", "Failed to sign in. please try again");
     } finally {
       setLoadingStrategy(null);
